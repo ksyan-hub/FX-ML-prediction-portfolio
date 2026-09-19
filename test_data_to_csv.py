d123c985f3a6fb387b95f6cefba76d35fcc3788b
kasyan <ksyunnpei@gmail.com>
2026-09-20 07:02:28 +0900

    xgboostで使うレートデータをmetatrader5から取得してcsvにして保存

--- test_data_to_csv.py
+++ test_data_to_csv.py
@@ -26,4 +26,4 @@ rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')
 print("\nDisplay dataframe with data")
 print(rates_frame)
 
-rates_frame.to_csv(r'C:\Users\bfgtu\OneDrive\デスクトップ\xgboost_練習_1\試作_1\TestDate_USDJPY_5m_20260713_20260720.csv',index=False)
+rates_frame.to_csv(r'.\TestDate_USDJPY_5m_20260713_20260720.csv',index=False)
