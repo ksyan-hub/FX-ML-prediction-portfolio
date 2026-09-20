f7e70250adeba755416772ad44b86e7ddcfa663d
kasyan <ksyunnpei@gmail.com>
2026-09-20 11:53:17 +0900

    データを取得してcsvとして保存

---
+++ testデータ取得_分足データto_csv.py
@@ -0,0 +1,33 @@
+from datetime import datetime  # 日付や時刻を扱うための標準ライブラリ
+
+import pandas as pd  # 表形式のデータを扱うためのライブラリ
+
+import pytz  # タイムゾーンを扱うためのライブラリ
+
+import MetaTrader5 as mt5
+
+if not mt5.initialize():
+    print("initialize() failed, error code =", mt5.last_error())
+    quit()
+
+start_day = (2026, 8, 11) # 取得開始日
+
+end_day = (2026, 8, 12) # 取得終了日
+
+timezone = pytz.timezone("Etc/UTC")
+utc_from = datetime(*start_day, tzinfo = timezone)
+utc_to = datetime(*end_day, tzinfo = timezone)
+rates = mt5.copy_rates_range("USDJPY.cl", mt5.TIMEFRAME_M5, utc_from, utc_to)
+mt5.shutdown()
+
+print("Display obained data")
+for rate in rates:
+    print(rate)
+
+rates_frame = pd.DataFrame(rates)
+rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')
+
+print("\nDisplay dataframe with data")
+print(rates_frame)
+
+rates_frame.to_csv(r'C:\Users\bfgtu\OneDrive\デスクトップ\xgboost_練習_1\試作_2\TestDate_USDJPY_5m_from2026, 8, 11_to2026, 8, 12.csv',index=False)
