5a47d58b69944fc452f7892db4a0f1fe87437b32
kasyan <ksyunnpei@gmail.com>
2026-09-21 17:58:49 +0900

    予測結果とMSEをCSVに記録する機能を追加
    予測と結果の誤差を二乗したSEとそれを過去12本分の平均MSEを算出して予測結果と一緒にcsvに保存できる機能を追加した。精度の分析がやり易くなり学習モデルの設計の見直しを試行錯誤している時期

---
+++ 03_se_mse_xgb_USDJPY_5m_.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+import csv
+from setting_traindata import data_setting
+
+train_data = data_setting()
+
+with open(train_data.train_data(), "r") as data:
+    train_df_USDJPY_5m =  pd.read_csv(data)
+
+window = 12
+ohlcts_cols = [ "open", "high", "low", "close", "tick_volume", "spread"]
+params = {"objective": "reg:squarederror", "learning_rate": 0.1,}
+booster = None
+predict_csv_file_path = rf".\predict_csv\試作_3_predicted_high.csv"
+se_buffer = []
+
+with open(predict_csv_file_path, "w", newline = "") as file:
+    writer = csv.writer(file)
+    writer.writerow(["0","predicted","actual","se","mse"])
+
+with open(predict_csv_file_path, "a", newline ="") as file:
+    writer = csv.writer(file)
+
+    for i in range(window, len(train_df_USDJPY_5m)):
+        window_data= train_df_USDJPY_5m.iloc[i - window: i][ohlcts_cols] 
+
+        if i % window == 0 :
+            print("window_data")
+            print(window_data)
+        window_data_shape = window_data.shape[0]
+
+        feature_row = window_data.values.flatten()#一次元配列に変換 
+
+        if i % window == 0:
+            print("feature_row")
+            print(feature_row) 
+
+        feature_columns = []
+
+        for j in range(window_data_shape):
+            for col in ohlcts_cols:
+                feature_columns.append(f"{col}_{j}")
+
+        train_x = pd.DataFrame([feature_row], columns = feature_columns)
+        train_y = pd.Series([train_df_USDJPY_5m.iloc[i]["high"]])
+        dtrain = xgb.DMatrix(train_x, label = train_y)#xgb.DMatrix(train_x, label=train_y)は特徴量とlabelの行数を一致させる必要がある
+        mse = None
+
+        if booster is not None:
+            predictions_data = booster.predict(dtrain)
+            predictions = float(predictions_data[0]) #スカラー値に変換
+            se = ((predictions - train_df_USDJPY_5m.iloc[i]["high"]))**2 #誤差を2乗
+            se_buffer.append(se)
+
+            if i % 12 == 0: #60分/5分＝12
+                sum_se = 0
+                count_se = len(se_buffer)
+
+                for val in se_buffer:
+                    sum_se = sum_se + val
+
+                mse= sum_se /  count_se
+                print(mse)
+                se_buffer = [] #bufferをリセット
+
+            else:mse = None
+
+            writer.writerow([ i, predictions, train_df_USDJPY_5m.iloc[i]["high"],se,mse])
+
+        booster = xgb.train(params, dtrain, num_boost_round = 1, xgb_model = booster)        
+
+        if i + 1 == len(train_df_USDJPY_5m):
+            print(f"{i}行目まで学習完了。{booster.num_boosted_rounds()}")
+            break
