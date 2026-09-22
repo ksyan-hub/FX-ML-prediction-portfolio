0d7335d3c63a47d0f1942cff89e5471f0d229ced
kasyan <ksyunnpei@gmail.com>
2026-09-22 18:17:37 +0900

    window分の時間足のデータをフラット化し特徴量に追加
    学習モデルにfor文を使ってデータを時系列ごとに取り出して渡している。window分の本数の特徴量から1本の高値を予測させる。実運用に一歩づつ近づいてはいるが、一本ごとに学習をしているためデータの傾向を決定木が学習できていないと、レヴューをもらったaiに指摘される。

--- 02_100feature_timeseries_xgb_USDJPY_5m_.py
+++ 02_100feature_timeseries_xgb_USDJPY_5m_.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import xgboost as xgb
-import matplotlib.pyplot as plt
+import csv
 
 train_csv_file = open(r".\TrainDate_USDJPY_5m_from2026, 8, 10_to2026, 8, 11.csv")
 train_df_USDJPY_5m = pd.read_csv(train_csv_file)
@@ -11,17 +11,35 @@ ohlcvr_cols = ["open", "high", "low", "close", "tick_volume", "real_volume"]
 feature_name = [f"{col}_{j}" for j in range(window) for col in ohlcvr_cols]
 params = {"objective": "reg:squarederror", "learning_rate": 0.1, }
 booster = None
+predict_buffer = []
+label_buffer = []
+
+with open(r".\predictions.csv", "w", newline = "")as file:
+    writer = csv.writer(file)
+    writer.writerow(["0", "predicted", "actual"])
 
 for i in range(window, len(train_df_USDJPY_5m)):
-    print(i)
     past_deta = train_df_USDJPY_5m.iloc[i-window:i][ohlcvr_cols]
     feature_row = past_deta.values.flatten()
+
     train_x = pd.DataFrame([feature_row], columns=feature_name)
     train_y = pd.Series([train_df_USDJPY_5m.iloc[i]["high"]])
     dtrain = xgb.DMatrix(train_x, label=train_y)
+
     predictions = booster.predict(dtrain) if booster is not None else None
-    print(predictions, train_y)
     booster = xgb.train(params, dtrain, num_boost_round=1, xgb_model=booster)
+
+    label = float(train_df_USDJPY_5m.iloc[i]["high"])
+    pred_value = float(predictions[0]) if predictions is not None else None
+    if not pred_value == None:
+        pred_value = int(pred_value*1000)/1000
+    label_buffer.append(label)
+    predict_buffer.append(pred_value)
+
+    if not pred_value == None:
+        with open(r".\predictions.csv", "a", newline = "")as file:
+            writer = csv.writer(file)
+            writer.writerow([i - 100, pred_value, label])
 
     if i % 200 == 0:
         print(f"{i}行目まで学習完了。{booster.num_boosted_rounds()}")
