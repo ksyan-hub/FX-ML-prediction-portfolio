a4d377b48ce8e893e3d71d47caa47808413c8dc5
kasyan <ksyunnpei@gmail.com>
2026-09-22 19:31:12 +0900

    予測結果とMSEをCSVに記録する機能を追加
    予測と結果の誤差を二乗したSEとそれを過去12本分の平均MSEを算出して予測結果と一緒にcsvに保存できる機能を追加した。精度の分析がやり易くなり学習モデルの設計の見直しを試行錯誤している時期

--- 03_se_mse_xgb_USDJPY_5m_.py
+++ 03_se_mse_xgb_USDJPY_5m_.py
@@ -5,7 +5,6 @@ import csv
 from setting_traindata import data_setting
 
 train_data = data_setting()
-
 with open(train_data.train_data(), "r") as data:
     train_df_USDJPY_5m =  pd.read_csv(data)
 
@@ -13,7 +12,7 @@ window = 12
 ohlcts_cols = [ "open", "high", "low", "close", "tick_volume", "spread"]
 params = {"objective": "reg:squarederror", "learning_rate": 0.1,}
 booster = None
-predict_csv_file_path = rf".\predict_csv\試作_3_predicted_high.csv"
+predict_csv_file_path = rf".\predict_csv\03_predicted_high.csv"
 se_buffer = []
 
 with open(predict_csv_file_path, "w", newline = "") as file:
@@ -25,20 +24,10 @@ with open(predict_csv_file_path, "a", newline ="") as file:
 
     for i in range(window, len(train_df_USDJPY_5m)):
         window_data= train_df_USDJPY_5m.iloc[i - window: i][ohlcts_cols] 
-
-        if i % window == 0 :
-            print("window_data")
-            print(window_data)
         window_data_shape = window_data.shape[0]
-
         feature_row = window_data.values.flatten()#一次元配列に変換 
 
-        if i % window == 0:
-            print("feature_row")
-            print(feature_row) 
-
         feature_columns = []
-
         for j in range(window_data_shape):
             for col in ohlcts_cols:
                 feature_columns.append(f"{col}_{j}")
@@ -46,8 +35,8 @@ with open(predict_csv_file_path, "a", newline ="") as file:
         train_x = pd.DataFrame([feature_row], columns = feature_columns)
         train_y = pd.Series([train_df_USDJPY_5m.iloc[i]["high"]])
         dtrain = xgb.DMatrix(train_x, label = train_y)#xgb.DMatrix(train_x, label=train_y)は特徴量とlabelの行数を一致させる必要がある
-        mse = None
 
+        mse = None
         if booster is not None:
             predictions_data = booster.predict(dtrain)
             predictions = float(predictions_data[0]) #スカラー値に変換
